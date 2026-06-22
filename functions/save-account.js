f9c682479319e0caa9f3d8821eea7e6cddc676ee
bbw4life <paulrenel99@gmail.com>
2026-06-22 00:22:31 -0400

    Update all pdg Francenel

--- functions/save-account.js
+++ functions/save-account.js
@@ -141,7 +141,7 @@ export async function onRequestPost(context) {
       return `${d.getDate().toString().padStart(2,'0')}/${(d.getMonth()+1).toString().padStart(2,'0')}/${d.getFullYear().toString().slice(-2)}`;
     }
 
-    const sheetData = await sheetsGet(accessToken, spreadsheetId, "bbw4life-accounts!A:Z");
+    const sheetData = await sheetsGet(accessToken, spreadsheetId, "bbw4life-accounts!A:AE");
     let rows = sheetData.values || [];
     const rowIndex = rows.findIndex(row => normalize(row[2] || "") === normalize(email));
     const rowNum = rowIndex + 1;
